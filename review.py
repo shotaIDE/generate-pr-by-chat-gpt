54d8c6556c1e9fdf561df19a83edfc510c613689
Shota Ide <ide.shota.dev@gmail.com>
2024-02-26 23:01:31 +0900

    fix

--- review.py
+++ review.py
@@ -1,40 +1,13 @@
 # coding: utf-8
 
-import os
 
-from programmer import Programmer
-from reviewer import Reviewer
+from reviewer import Reviewer2
 
 
 def work():
-    prompt = os.environ.get('PROMPT')
-    if prompt is None:
-        issue_title = os.environ.get('ISSUE_TITLE')
-        if issue_title is None:
-            raise Exception('PROMPT or ISSUE_TITLE must be set')
+    reviewer = Reviewer2(prompt='prompt')
 
-        issue_description = os.environ.get('ISSUE_DESCRIPTION')
-        prompt = (
-            f'title: {issue_title}\n'
-            f'description: {issue_description}'
-        )
-
-    print(f'Engineer leader: {prompt}')
-
-    programmer = Programmer(prompt=prompt)
-    reviewer = Reviewer(prompt=prompt)
-
-    reviewer_comment = None
-    iteration = 0
-
-    while iteration < 10:
-        programmer_comment = programmer.work(reviewer_comment=reviewer_comment)
-        reviewer_comment = reviewer.work(programmer_comment=programmer_comment)
-
-        if reviewer.lgtm():
-            break
-
-        iteration += 1
+    reviewer_comment = reviewer.work()
 
 
 if __name__ == "__main__":
